97082b3bc3ce8d9a53859820dac4cbc229983ede
jacocanete <jacogagarincanete@gmail.com>
2025-03-06 15:26:56 +0800

    Add chapter 3, L2 files

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-from stats import get_num_char
-
-
-def get_book_text(path):
-    with open(path) as f:
-        file_contents = f.read()
-    return file_contents
+from stats import print_stats, get_num_char, get_book_text
 
 
 def main():
-    file_contents = get_book_text("books/frankenstein.txt")
-    letter_count = get_num_char(file_contents)
-    print(f"{letter_count} words found in the document")
+    path = "books/frankenstein.txt"
+    print_stats(path)
 
 
 main()
--- stats.py
+++ stats.py
@@ -14,3 +14,38 @@ def get_num_char(file_contents):
             char_count[char] = 1
 
     return char_count
+
+
+def get_book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def get_num(d):
+    return d["num"]
+
+
+def dict_to_sorted_list(char_count):
+    sorted_list = []
+    for char in char_count:
+        sorted_list.append({"char": char, "num": char_count[char]})
+    sorted_list.sort(key=get_num, reverse=True)
+    return sorted_list
+
+
+def print_stats(path):
+    file_contents = get_book_text(path)
+    word_count = get_num_words(file_contents)
+    char_count = get_num_char(file_contents)
+    sorted_list = dict_to_sorted_list(char_count)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
